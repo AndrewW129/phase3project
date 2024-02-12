67f5a6d065fdd5520ee7492e1f0c6dc93a4f7bec
Andrew Wilson <adwilson12901@gmail.com>
2024-02-12 16:27:01 -0500

    initial commit

---
+++ server/migrations/versions/f1ae08414791_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: f1ae08414791
+Revises: 
+Create Date: 2024-02-12 16:26:59.736450
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f1ae08414791'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
